5b19a148a4bc59f970e034cd85959f7290a42b25
Sd2labs_frontend <sd2labs_frontend@Sd2labs-frontends-Mac-mini-2.local>
2019-11-30 12:02:01 +0530

    URL issue fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
 	mount Github::Base => "/"
 
-	get '/score/:username', to: redirect('/api/v1/score/:username')
+	match "/score/:username" => redirect { |params, request| "/api/v1/score/#{params[:username]}" }, :via=>[:get]
 
 end
